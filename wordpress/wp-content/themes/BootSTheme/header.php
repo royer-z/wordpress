2bce42a359120d46ced82c13646489cd4a52ae9d
Royer Zamudio <rvz2@njit.edu>
2019-03-06 13:14:18 -0500

    Task: split index.php to header.php and footer.php

---
+++ wordpress/wp-content/themes/BootSTheme/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap WordPress Theme</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo get_stylesheet_uri(); ?>
+</head>
+<body>
+
+<div class = "navbar navbar-inverse navbar-static-top">
--- wordpress/wp-content/themes/BootSTheme/index.php
+++ wordpress/wp-content/themes/BootSTheme/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Bootstrap WordPress Theme</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
+<?php get_header(); ?>
 
-<div class = "navbar navbar-inverse navbar-static-top">
     <div class = "container">
 
         <a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
@@ -81,69 +74,4 @@
 
 </div>
 
-<div class = "navbar navbar-default navbar-fixed-bottom">
-
-    <div class = "container">
-        <p class = "navbar-text pull-left">Site Built By Ahsan</p>
-        <a href = "http://youtube.com/" class = "navbar-btn btn-danger btn pull-right">Subscribe on YouTube</a>
-    </div>
-
-</div>
-
-<div class = "modal fade" id = "contact" role = "dialog">
-    <div class = "modal-dialog">
-        <div class = "modal-content">
-            <form class = "form-horizontal">
-                <div class = "modal-header">
-                    <h4>Contact BootStrap Theme</h4>
-                </div>
-                <div class = "modal-body">
-
-                    <div class = "form-group">
-
-                        <label for = "contact-name" class = "col-lg-2 control-label">Name:</label>
-                        <div class = "col-lg-10">
-
-                            <input type = "text" class = "form-control" id = "contact-name" placeholder = "Full Name">
-
-                        </div>
-
-                    </div>
-
-                    <div class = "form-group">
-
-                        <label for = "contact-email" class = "col-lg-2 control-label">Email:</label>
-                        <div class = "col-lg-10">
-
-                            <input type = "email" class = "form-control" id = "contact-email" placeholder = "you@example.com">
-
-                        </div>
-
-                    </div>
-
-                    <div class = "form-group">
-
-                        <label for = "contact-msg" class = "col-lg-2 control-label">Message:</label>
-                        <div class = "col-lg-10">
-
-                            <textarea class = "form-control" rows = "8"></textarea>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                <div class = "modal-footer">
-                    <a class = "btn btn-default" data-dismiss = "modal">Close</a>
-                    <button class = "btn btn-primary" type = "submit">Send</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-
-
-</body>
-</html>
+<?php get_footer(); ?>
