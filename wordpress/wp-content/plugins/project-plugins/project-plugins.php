10623d4cb1cb0f675f6eaa13860cbc18e914c351
tmti20 <tmti20@gmail.com>
2019-03-13 01:25:38 -0400

    Task: New plugins ShortText code

--- wordpress/wp-content/plugins/project-plugins/project-plugins.php
+++ wordpress/wp-content/plugins/project-plugins/project-plugins.php
@@ -1,19 +1,27 @@
 <?php
 /**
-* Plugin Name: Very First Plugin
-* Plugin URI: https://www.yourwebsiteurl.com/
-* Description: This is the very first plugin I ever created.
+* Plugin Name: ShortText
+* Plugin URI: https://www.test.com/
+* Description: This is the very first plugin I ever created. With this plugins you will be make shortcut for you text.
 * Version: 1.0
-* Author: Your Name Here
-* Author URI: http://yourwebsiteurl.com/
+* Author: Tanvirul Islam
+* Author URI: http://tmsclick.com/
 **/
-
-function dh_modify_read_more_link() {
-
-    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
-
+//ShortText functions
+function shortText(){
+    $text = " The New Jersey Institute of Technology (NJIT) is a public research university (Carnegie, R1) in Newark, New Jersey. Located within 30 minutes of Manhattan by rapid transit, NJIT is New Jersey's Science & Technology University.[b][15] Among other initiatives, it operates the Enterprise Development Center (EDC), one of the largest tech-oriented business incubators in the country.";
+    return $text;
 }
 
-add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+add_shortcode('about_njit', 'shortText');
 
-?>
+//function dh_modify_read_more_link() {
+//
+//    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+//
+//}
+//
+//add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+//
+//?>
+
